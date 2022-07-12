9571a9b1a426214c35d2aaa1a78b5d19f94659aa
GoodSwimmer <54301924+GoodSwimmer@users.noreply.github.com>
2022-07-12 11:02:00 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ var header = $("#fixedContent");
           header.removeClass("fixed");
         }
 });
+
+$(window).scroll(function(){
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+
+/*win.scroll(function(){
+  scrollPosition = win.scrollTop();
+  scrollRatio = 1 - scrollPosition / 300;
+  $(".top").css("opacity", scrollRatio);
+*/
